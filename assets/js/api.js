1b8c145d6995caeac811cdbc1ed806451acbcda6
Lais_lfs <laislara86@gmail.com>
2025-01-22 15:53:55 -0300

    Alterando link da api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = "https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/refs/heads/main/data/profile.json";
+    const url = "https://raw.githubusercontent.com/Lais-lfs/Portfolio/refs/heads/main/data/profile.json";
     const fetching = await fetch(url);
     return await fetching.json();
 }
